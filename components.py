a1fa8771d444d5c70b96ebda763de7a5a3cb5d03
CezarySzukiel <cezary.szukiel@gmail.com>
2025-01-27 21:26:47 +0100

    Interface and components

--- components.py
+++ components.py
@@ -1,6 +1,100 @@
-class HealthComponent:
-    def __init__(self, health: int):
-        self.health = health
+from interface import *
+
+
+class ObjectComponent(ObjectInterface):
+    def __init__(self, position: list[int, int] = [0, 0], area: tuple[int, int] = (1, 1)):
+        self._area = area
+        self._position = position
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        self._position = value
+
+    @property
+    def area(self):
+        return self._area
+
+    @area.setter
+    def area(self, value):
+        self._area = value
+
+
+class WalkableComponent(ObjectComponent, WalkableInterface):
+    def __init__(self):
+        super().__init__()
+        self._walkability = True
+
+    @property
+    def walkability(self):
+        return self._walkability
+
+    @walkability.setter
+    def walkability(self, value):
+        self._walkability = value
+
+
+class SwimableComponent(ObjectComponent, SwimableInterface):
+    def __init__(self):
+        super().__init__()
+        self._swimability = True
+
+    @property
+    def swimability(self):
+        return self._swimability
+
+    @swimability.setter
+    def swimability(self, value):
+        self._swimability = value
+
+
+class InteractableObjectComponent(ObjectComponent, InteractableObjectInterface):
+    def __init__(self, interaction: bool = True):
+        super().__init__()
+        self._interaction = interaction
+
+    @property
+    def interaction(self):
+        return self._interaction
+
+    @interaction.setter
+    def interaction(self, value):
+        self._interaction = value
+
+
+class LifeObjectComponent(InteractableObjectComponent, LifeObjectInterface):
+    def __init__(self, max_health: int = 50, health: float = 50, player: str = "Gaia"):
+        super().__init__()
+        self._max_health = max_health
+        self._health = health
+        self._player = player
+
+    @property
+    def max_health(self):
+        return self._max_health
+
+    @max_health.setter
+    def max_health(self, value):
+        self._max_health = value
+
+    @property
+    def health(self):
+        return self._health
+
+    @health.setter
+    def health(self, value):
+        self._health = value
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, value):
+        self._player = value
 
     def take_damage(self, damage: int):
         self.health -= damage
@@ -9,30 +103,86 @@ class HealthComponent:
         return self.health
 
 
-class MovementComponent:
-    def __init__(self, speed: int):
-        self.speed = speed
+class MobileObjectComponent(LifeObjectComponent, MobileObjectInterface):
+    def __init__(self, speed: int, direction: str = "N"):
+        super().__init__()
+        self._direction = direction
+        self._speed = speed
 
-    def move(self, dx: int, dy: int):
-        print(f"Moving by ({dx}, {dy}) with speed {self.speed}")
+    @property
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, value):
+        self._speed = value
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, value):
+        self._direction = value
+
+    def turn(self, value):
+        self._direction = value
+        print("Object turned")
 
 
-class CombatComponent:
-    def __init__(self, attack_damage: int):
-        self.attack_damage = attack_damage
+class ResourceComponent(InteractableObjectComponent, ResourceInterface):
+    def __init__(self, resource_type: str, amount: int):
+        super().__init__()
+        self._amount = amount
+        self._resource_type = resource_type
 
-    def attack(self, target: HealthComponent):
-        print(f"Attacking target with {self.attack_damage} damage")
-        target.take_damage(self.attack_damage)
+    @property
+    def amount(self):
+        return self._amount
 
+    @amount.setter
+    def amount(self, value: int):
+        self._amount = value
 
-class ResourceComponent:
-    def __init__(self, resource_type: str, reserves: int):
-        self.resource_type = resource_type
-        self.reserves = reserves
+    @property
+    def resource_type(self):
+        return self._resource_type
 
-    def gather(self, amount: int):
-        gathered = min(amount, self.reserves)
-        self.reserves -= gathered
+    @resource_type.setter
+    def resource_type(self, value: str):
+        self._resource_type = value
+
+    def get_gathered(self, amount: int):
+        gathered = min(amount, self.amount)
+        self.amount -= gathered
         print(f"Gathered {gathered} {self.resource_type}")
         return gathered
+
+
+class CombatComponent(CombatInterface):
+    def __init__(self, damage: int, attack_range: int = 1):
+        self._damage = damage
+        self._range = attack_range
+
+    @property
+    def attack_range(self):
+        return self._range
+
+    @attack_range.setter
+    def attack_range(self, value):
+        self._range = value
+
+    @property
+    def damage(self):
+        return self._damage
+
+    @damage.setter
+    def damage(self, value):
+        self._damage = value
+
+    def attack(self, target):
+        print(f"Attacking {target}")
+        target.take_damage(self.damage)
+        return target
+
+
--- interface.py
+++ interface.py
@@ -1,42 +1,53 @@
 from abc import abstractmethod, ABC
 
-from surfaces import Surface
 
-
-class SurfaceInterface(ABC):
-    """
-    Interface for all surfaces
-    """
-
-    @property
-    @abstractmethod
-    def walkability(self) -> bool:
-        """walkability"""
-        pass
-
-    @property
-    @abstractmethod
-    def swimability(self) -> bool:
-        """swimability"""
-        pass
-
-    @abstractmethod
-    def position(self) -> tuple[int, int]:
-        """position"""
-        pass
-
-
-class ObjectInterface(SurfaceInterface):
+class ObjectInterface(ABC):
     """
     Base interface for all objects
     """
 
     @abstractmethod
-    def area(self):
+    def position(self) -> list[int, int]:
         pass
 
     @abstractmethod
+    def area(self) -> tuple[int, int]:
+        pass
+
+
+class WalkableInterface(ObjectInterface):
+    """
+    Interface for all walkable objects
+    """
+
+    @property
+    @abstractmethod
+    def walkability(self) -> bool:
+        """Walking units can walk over me"""
+        pass
+
+
+class SwimableInterface(ObjectInterface):
+    """
+    Interface for all swimable objects
+    """
+
+    @property
+    @abstractmethod
+    def swimability(self) -> bool:
+        """Swimming units can swim over me"""
+        pass
+
+
+class InteractableObjectInterface(ObjectInterface):
+    """
+    Interface for all interactable objects
+    """
+
+    @property
+    @abstractmethod
     def interaction(self) -> bool:
+        """interaction"""
         pass
 
 
@@ -47,19 +58,25 @@ class LifeObjectInterface(ObjectInterface):
 
     @property
     @abstractmethod
-    def hp(self) -> int:
+    def max_health(self) -> int:
         """health points"""
         pass
 
     @property
     @abstractmethod
-    def speed(self) -> float:
-        """speed of object"""
+    def health(self) -> int:
+        """health points"""
         pass
 
     @abstractmethod
     def take_damage(self, damage: int):
         """when object is attacked"""
+        pass
+
+    @property
+    @abstractmethod
+    def player(self):
+        """Defines which player the object belongs to"""
         pass
 
 
@@ -68,8 +85,43 @@ class MobileObjectInterface(LifeObjectInterface):
     Base interface for all mobile objects
     """
 
+    @property
     @abstractmethod
-    def move(self):
+    def speed(self) -> float:
+        """speed of object"""
+        pass
+
+    @property
+    @abstractmethod
+    def direction(self) -> str:
+        """direction of object"""
+        pass
+
+    @abstractmethod
+    def turn(self, value: str) -> None:
+        """Object can turn around"""
+        pass
+
+
+class WalkingObjectInterface(MobileObjectInterface):
+    """
+    Interface for all walking objects
+    """
+
+    @abstractmethod
+    def walk(self):
+        """Object walking"""
+        pass
+
+
+class SwimmingObjectInterface(MobileObjectInterface):
+    """
+    Interface for all swimming objects
+    """
+
+    @abstractmethod
+    def swim(self):
+        """Object swimming"""
         pass
 
 
@@ -96,138 +148,24 @@ class ResourceInterface(ObjectInterface):
         pass
 
 
-class Resource(ResourceInterface):
+class CombatInterface(ABC):
     """
-    Resource
-    """
-
-    def __init__(self, position, walkability, swimability, interaction, area, amount, resource_type):
-        super().__init__()
-        self._position = (0, 0)
-        self._walkability = False
-        self._swimability = False
-        self._interaction = True
-        self._area = area
-        self._amount = amount
-        self._resource_type = "gold"
-
-    @property
-    def position(self):
-        return self._position
-
-    @position.setter
-    def position(self, value: tuple[int, int]):
-        self._position = value
-
-    @property
-    def walkability(self):
-        return self._walkability
-
-    @walkability.setter
-    def walkability(self, value: bool):
-        self._walkability = value
-
-    @property
-    def swimability(self):
-        return self._swimability
-
-    @swimability.setter
-    def swimability(self, value):
-        self._swimability = value
-
-    @property
-    def interaction(self):
-        return self._interaction
-
-    @interaction.setter
-    def interaction(self, value: bool):
-        self._interaction = value
-
-    @property
-    def area(self):
-        return self._area
-
-    @area.setter
-    def area(self, value: tuple[int, int]):
-        self._area = value
-
-    @property
-    def amount(self):
-        return self._amount
-
-    @amount.setter
-    def amount(self, value: int):
-        self._amount = value
-
-    @property
-    def resource_type(self):
-        return self._resource_type
-
-    @resource_type.setter
-    def resource_type(self, value: str):
-        self._resource_type = value
-
-    def get_gathered(self, quantity: int) -> None:
-        gathered = min(quantity, self.amount)
-        self.amount -= gathered
-        print(f"Gathered {gathered} {self.resource_type}")
-
-
-class LifeObject(LifeObjectInterface):
-    """
-    Base class for all living objects
+    Interface for all combat objects
     """
 
-    # todo trzeba by implementować ponownie metody które ma już klasa Resource, takie jak position, walkability, swimability, interaction, area. jak to zrobić poprawnie by nie łamać DRY?
-    # todo czy da się w tym celu użyć kompozycji?
-    def __init__(self, hp, speed):
-        self._hp = hp
-        self._speed = speed
+    @property
+    @abstractmethod
+    def damage(self) -> int:
+        """damage dealt by object"""
+        pass
 
     @property
-    def hp(self):
-        return self._hp
+    @abstractmethod
+    def attack_range(self) -> int:
+        """range of attack"""
+        pass
 
-    @hp.setter
-    def hp(self, value: int):
-        self._hp = value
-
-    @property
-    def speed(self):
-        return self._speed
-
-    @speed.setter
-    def speed(self, value: float):
-        self._speed = value
-
-    def take_damage(self, damage: int):
-        self.hp -= damage
-        if self.hp <= 0:
-            print("Object destroyed!")
-        return self.hp
-
-
-class Animal(Resource):
-    """
-    All animals who can not fight.
-    (deer, sheep)
-    """
-
-    def __init__(self):
-        super().__init__()
-
-    # todo dziedziczenie plus kompozycja? resource + lifeObject + mobileObject
-    # czy nie powinienem najpierw zrobić AnimalInterface
-
-
-gold = Resource(position=(0, 0), walkability=False, swimability=False, interaction=True, area=(2, 2), amount=800,
-                resource_type="gold")
-
-print(vars(gold))
-# # todo zastosować gdzieś kompozycję
-# todo klasy interface zrobić abstrakcyjne
-# todo wilk będzie żołnierzem gracza przyroda, bo nie dało się z niego pozyskiwać jedzenia
-# todo klasa AnimalFighter, bo dzik atakował, poza tym niektóre budynki też atakowały, więc nie fighter osobno
-
-#
-# print(dir())
+    @abstractmethod
+    def attack(self, target: LifeObjectInterface) -> None:
+        """attacking"""
+        pass
