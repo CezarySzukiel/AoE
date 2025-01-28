3518fddc7539c6e3061db4e18b48689a1faf69c5
CezarySzukiel <cezary.szukiel@gmail.com>
2025-01-28 12:39:16 +0100

    surfaces classes

--- surfaces.py
+++ surfaces.py
@@ -1,69 +1,31 @@
-from abc import ABC
+from components import WalkableComponent, SwimableComponent
 
 
-class Surface(ABC):
-    """
-    Surface like ground, or water
-    """
-
-    def __init__(self, walkability: bool, swimability: bool, position_x: int | None = None,
-                 position_y: int | None = None):
-        self.walkability = walkability
-        self.swimability = swimability
-        self.position_x = position_x
-        self.position_y = position_y
-
-    def describe(self):
-        """
-        Describe method
-        """
-        return f'I am a {self.__class__} !'
-
-
-class Ground(Surface):
+class Ground(WalkableComponent):
     """
     Ground
     """
 
-    def __init__(self, walkability: bool = True, swimability: bool = False):
-        super().__init__(walkability=walkability, swimability=swimability)
+    def __init__(self, ):
+        super().__init__()
 
 
-class Water(Surface):
+class Water(SwimableComponent):
     """
     Water
     """
 
-    def __init__(self, walkability: bool = False, swimability: bool = True):
-        super().__init__(walkability=walkability, swimability=swimability)
+    def __init__(self):
+        super().__init__()
 
 
-class ImpassableSurface(Surface):
-    """
-    Impassable surface
-    """
-
-    def __init__(self, walkability: bool = False, swimability: bool = False):
-        super().__init__(walkability=walkability, swimability=swimability)
-
-
-class Shoal(Surface):
+class Shoal(SwimableComponent, WalkableComponent):
     """
     Shoal
     """
 
-    def __init__(self, walkability: bool = True, swimability: bool = True):
-        super().__init__(walkability=walkability, swimability=swimability)
-
-
-class Mud(Ground):
-    """
-    Mud
-    """
-
-    def __init__(self, movement_bonus=0.8):
+    def __init__(self):
         super().__init__()
-        self.movement_bonus = movement_bonus
 
 
 class Path(Ground):
